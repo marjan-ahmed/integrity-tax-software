812b683b2173113db26618696c96f89567b2bedf
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-09-08 15:00:02 +0500

    refactor: simplify next.config.mjs by removing unnecessary comments and whitespace

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.leadconnectorhq.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'assets.cdn.filesafe.space',
-      },
-      {
-        protocol: 'https',
-        hostname: 'framerusercontent.com'
-      }
+      { protocol: 'https', hostname: 'images.leadconnectorhq.com' },
+      { protocol: 'https', hostname: 'assets.cdn.filesafe.space' },
+      { protocol: 'https', hostname: 'framerusercontent.com' }
     ],
   },
 };
-
 export default nextConfig;
